032e86cce184e09513ba65d69cf9a3dc8e800eab
Slime <49380948+SL9M@users.noreply.github.com>
2025-06-22 22:14:53 -0700

    Erase command logic added to step 2
    Erase command implemented and working
    
    includes instructions for when to plug in the probe.

--- src/step2.py
+++ src/step2.py
@@ -1,9 +1,31 @@
 from PyQt6.QtWidgets import  QLabel, QFileDialog, QPushButton, QLineEdit
 from src.config import openocdpath
+import subprocess
 #Step 2 - erase
 
 def step2(layout):
 
+    # Step 2 variables
     step2title= QLabel("Step 2: Erase Device")
     step2title.setStyleSheet("font-size:20px; font-weight:bold;")
+    step2instructions = QLabel('Plug in the wavephoenix you would like to flash, then click "Erase"')
+    eraseButton = QPushButton("Erase Device")
+
+    def erase_device():
+        try:
+            eraseCommand = [
+                openocdpath,
+                "-f", "interface\\cmsis-dap.cfg",
+                "-f", "target\\efm32s2.cfg",
+                "-c", "init; efm32s2_dci_device_erase; shutdown"
+            ]
+            eraseResult = subprocess.run(eraseCommand, capture_output=True, text=True, check=True)
+            print("Erase Success\n",eraseResult.stdout)
+        except subprocess.CalledProcessError as eraseError:
+            print("Erase Error\n", eraseError.stderr)
+
+    # Create layout
     layout.addWidget(step2title)
+    layout.addWidget(step2instructions)
+    layout.addWidget (eraseButton)
+    eraseButton.clicked.connect(erase_device)
